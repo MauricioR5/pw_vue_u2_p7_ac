98a1a8d92b8dffdb7beabc3841f645b210b4c91f
Mauricio <amcacuangoa@uce.edu.ec>
2024-12-20 00:47:07 +0000

    Taller_18

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <Contador />
-  <Contador />
+  <Contador 
+  encabezado="ContAdor Primero" 
+  :valor="calcular(1, 2)" 
+  :esVerdad="muestra1" 
+  :valor2="70" />
+
+  <Contador 
+  encabezado=" Segundo" 
+  :valor="calcular(5, 4)" 
+  :esVerdad="muestra2"/>
+
+  <button @:click="cambiar(1)">Cambiar Primero</button>
+  <button @:click="cambiar(2)">Cambiar Segundo</button>
 </template>
 
 <script>
@@ -9,9 +20,30 @@ import Contador from './components/Contador.vue'
 export default {
   name: 'App',
   components: {
-   Contador
+    Contador
+  },
+  methods: {
+    calcular(a, b) {
+      return a + b;
+    },
+    cambiar(valor) {
+      if(valor===1){
+        this.muestra1 = !this.muestra1;
+      }else{
+        this.muestra2 = !this.muestra2;
+    }
+    this.muestra = !this.muestra;
   }
-}
+  },
+  data() {
+    return {
+      muestra:true,
+      muestra1: true,
+      muestra2: true,
+
+    };
+  },
+};
 </script>
 
 <style>
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,58 +1,92 @@
 <template>
-    <h2>{{titulo}}</h2>
-   
-    <p>{{ numero }} <sup>2</sup> = {{calcularCuadradoComputado}}</p>
-    <p>{{ numero }} <sup>2</sup> = {{calcularCuadradoComputado}}</p>
+    <h2>{{ encabezado }} :{{ valor2 }}</h2>
+
+    <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputado }}</p>
+    <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputado }}</p>
     <div>
-      <button v-on:click="incrementar">+1</button>
-      <button v-on:click="decrementar">-1</button>
+        <button @:click="incrementar">+1</button> <!-- @:click es lo mismo que v-on:click -->
+        <button v-on:click="decrementar">-1</button>
     </div>
-   
-   
-   
-  </template>
-   
-  <script>
-  export default {
-      data(){
-          return{
-              numero:10,
-              titulo:'Contador',
-          }
-      },
-      methods:{
-          calcularCuadrado(){
-              console.log("Entro a calcular")
-              return this.numero*this.numero;
-          },
-          incrementar(){
-              this.numero++;
-          },
-          decrementar(){
-              this.numero--;
-          }
-      },
-      computed:{
-          calcularCuadradoComputado(){
-              console.log("Entro a calcular computado")
-              return this.numero*this.numero;
-          }
-      }
-  }
-  </script>
-   
-  <style >
-   button{
-       background-color: #07700a;
-       border-radius: 5px;
-       color: white;
-       padding: 10px 10px;
-       text-align: center;
-       text-decoration: none;
-       display: inline-block;
-       font-size: 16px;
-   }
-   button:hover{
-       background-color: #053808;
-   }
-  </style>
+    <div v-if="esVerdad">
+        <h1>Feliz Navidad</h1>
+    </div>
+
+
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            numero: this.valor,
+            titulo: 'Contador',
+        }
+    },
+    methods: {
+        calcularCuadrado() {
+            console.log("Entro a calcular")
+            return this.numero * this.numero;
+        },
+        incrementar() {
+            this.numero++;
+        },
+        decrementar() {
+            this.numero--;
+        }
+    },
+    computed: {
+        calcularCuadradoComputado() {
+            console.log("Entro a calcular computado")
+            return this.numero * this.numero;
+        }
+    },
+    //Primera forma de declarar un props
+    //props: ['encabezado','valor']
+    
+    //Segunda forma de declarar un props
+    props:{
+        encabezado:{
+            type: String,
+            validator: function (value) {
+                return !value.includes('A');
+            }
+        }, 
+        valor:Number,
+        valor2:{
+            type:Number,
+            required:false,
+            default:77,
+            validator(value){
+                //Programo bajo mi criterio y retorno true y retorno false cuando no es válido pa mi
+                return value <= 77;
+            }
+        },
+        esVerdad:{
+            type:Boolean,
+            required:true
+        },
+        arreglo:Array,
+        objetoPersona:Object, 
+        fecha: Date,       
+        
+    }
+}
+</script>
+
+<style>
+button {
+    background-color: #07700a;
+    border-radius: 5px;
+    color: white;
+    padding: 10px 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+}
+
+button:hover {
+    background-color: #053808;
+}
+</style>
